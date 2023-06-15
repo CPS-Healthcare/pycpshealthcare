04e97bbfdb964a7324c4fb77770ecd9cdc477ed5
Fernando Huanca <fjhuanca@uc.cl>
2023-06-15 02:39:09 -0400

    change accel magnitude format

--- pycpshealthcare/database/utils.py
+++ pycpshealthcare/database/utils.py
@@ -52,9 +52,11 @@ def generate_vector_magnitude_pipeline(id_match, timestamp_start=None, timestamp
         {"$project": {
             "timestamp": 1,
             "test_id": 1,
-            "magnitude": {
-                "$sqrt": {
-                    "$sum": [{"$pow": [ { "$toDouble": f"$values.{coord}" }, 2 ]} for coord in coords]
+            "values":{
+                "accel_magnitude": {
+                    "$sqrt": {
+                        "$sum": [{"$pow": [ { "$toDouble": f"$values.{coord}" }, 2 ]} for coord in coords]
+                        }
                     }
                 }
             }
@@ -84,9 +86,9 @@ def generate_vector_stats_magnitude_pipeline(id_match, bin_size=60, bin_unit="mi
             }
         }
     ]
-    group_pipe = {f"mag_{oper}": {f"${oper}": f"$magnitude"} for oper in ["avg", "min", "max"]}
+    group_pipe = {f"mag_{oper}": {f"${oper}": f"$values.accel_magnitude"} for oper in ["avg", "min", "max"]}
     pipeline[2]["$group"].update(group_pipe)
     # project_pip = {f"{oper}_{i}": 1 for i in types for oper in ["avg", "min", "max"]}
-    project_pip = {f"{oper}": {"magnitude": f"$mag_{oper}"}  for oper in ["avg", "min", "max"]}
+    project_pip = {f"{oper}": {"accel_magnitude": f"$mag_{oper}"}  for oper in ["avg", "min", "max"]}
     pipeline[3]["$project"].update(project_pip)
     return pipeline
--- setup_readme.md
+++ setup_readme.md
@@ -6,3 +6,4 @@ pip install https://github.com/CPS-Healthcare/pycpshealthcare/releases/download/
 pip install https://github.com/CPS-Healthcare/pycpshealthcare/releases/download/v1.1.0-alpha/pycpshealthcare-1.1.0a0-py2.py3-none-any.whl
 pip install https://github.com/CPS-Healthcare/pycpshealthcare/releases/download/v1.2.0-alpha/pycpshealthcare-1.2.0a0-py2.py3-none-any.whl
 pip install https://github.com/CPS-Healthcare/pycpshealthcare/releases/download/v1.3.0-alpha/pycpshealthcare-1.3.0a0-py2.py3-none-any.whl
+pip install https://github.com/CPS-Healthcare/pycpshealthcare/releases/download/v1.4.0/pycpshealthcare-1.4.0-py2.py3-none-any.whl
